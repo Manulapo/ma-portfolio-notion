efdb84546e049725c9659ca64e77d2057aa3eb60
Manulapo <manulaporta98@gmail.com>
2026-01-10 22:01:17 +0100

    routing handling

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,36 +4,36 @@ import type { RouteRecordRaw } from 'vue-router'
 const baseName = import.meta.env.VITE_SITE_BASE || ''
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () => import('../views/About.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(baseName),
-  routes
+    history: createWebHistory(baseName),
+    routes
 })
 
 // Handle redirects from 404.html
 router.beforeEach((to, _from, next) => {
-  const redirectPath = new URLSearchParams(window.location.search).get('path')
-  
-  if (redirectPath && redirectPath.includes(baseName)) {
-    const targetPath = redirectPath.split('/').pop()
-    if (targetPath && targetPath !== to.path.substring(1)) {
-      next('/' + targetPath)
-      return
+    const redirectPath = new URLSearchParams(window.location.search).get('path')
+
+    if (redirectPath && redirectPath.includes(baseName)) {
+        const targetPath = redirectPath.split('/').pop()
+        if (targetPath && targetPath !== to.path.substring(1)) {
+            next('/' + targetPath)
+            return
+        }
     }
-  }
-  
-  next()
+
+    next()
 })
 
 export default router
