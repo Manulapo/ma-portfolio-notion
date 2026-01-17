b5c63838a7732e0a72ceb42d9f297eb867e2304c
Manulapo <manulaporta98@gmail.com>
2026-01-17 12:25:57 +0100

    Updates

---
+++ assets/About-2rjjYnXJ.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./About.vue_vue_type_script_setup_true_lang-CJSk6JEE.js";import"./index-C4xVc9Jl.js";export{m as default};
--- assets/About-CX_cZLlM.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./About.vue_vue_type_script_setup_true_lang-BIL3cQdF.js";import"./index-5MDv0JaH.js";export{m as default};
