60fa842dc1d79e50b1bdc53be160039d82dfa89b
Manulapo <manulaporta98@gmail.com>
2026-01-18 20:55:35 +0100

    deleted redirect

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,12 +10,6 @@ const routes: RouteRecordRaw[] = [
         component: () => import('../views/Home.vue')
     },
     {
-        path: '/about',
-        name: 'About',
-        component: () => import('../views/About.vue')
-    }
-    ,
-    {
         path: '/:pathMatch(.*)*',
         name: 'NotFound',
         component: () => import('../views/404.vue')
@@ -25,21 +19,6 @@ const routes: RouteRecordRaw[] = [
 const router = createRouter({
     history: createWebHistory(baseName),
     routes
-})
-
-// Handle redirects from 404.html
-router.beforeEach((to, _from, next) => {
-    const redirectPath = new URLSearchParams(window.location.search).get('path')
-
-    if (redirectPath && redirectPath.includes(baseName)) {
-        const targetPath = redirectPath.split('/').pop()
-        if (targetPath && targetPath !== to.path.substring(1)) {
-            next('/' + targetPath)
-            return
-        }
-    }
-
-    next()
 })
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
@@ -9,15 +8,7 @@ export default defineConfig(({ command }) => {
 
   return {
     plugins: [
-      vue(),
-      viteStaticCopy({
-        targets: [
-          {
-            src: './src/assets/404.html',
-            dest: './'
-          }
-        ]
-      })
+      vue()
     ],
     base,
     define: {
